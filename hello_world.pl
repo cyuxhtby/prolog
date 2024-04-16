68e8929584ff5f1de09ad4271c9b469a4d71998f
Diego Aguilar <67769941+cyuxhtby@users.noreply.github.com>
2024-04-15 20:26:38 -0500

    init

---
+++ hello_world.pl
@@ -0,0 +1,4 @@
+% predicates are defined using the format: name :- body
+hello_world :-
+    write('Hello, World!'), % write to console
+    nl. % new line, with `.` being the end of the predicate definition
