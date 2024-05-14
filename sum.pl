a402f5f409e108cb6976f404e27e9729af4cad23
Diego Aguilar <67769941+cyuxhtby@users.noreply.github.com>
2024-05-13 20:49:51 -0500

    doc: minor fix

--- sum.pl
+++ sum.pl
@@ -1,10 +1,10 @@
 % defines the sum/2 predicate
 
-sum([], 0). % base case for empty list, argument S gets bound to 0 as starting value
-% for non-empty list, bind 
-sum([Head | Tail], Sum):- 
+sum([], 0). % base case for empty list. when list is empty, sum is zero
+% for non-empty list, bind recursive sum to S
+sum([Head | Tail], S):- 
     sum(Tail, SumPrev),
-    Sum is Head + SumPrev.
+    S is Head + SumPrev.
 
 
 ?- sum([10, 3, 5], X). % returns 18
